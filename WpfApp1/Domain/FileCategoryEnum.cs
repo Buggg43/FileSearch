5ec7d8e639bb3279fd1280ae36080fabe0062b68
Buggg43 <kacperwerra12@gmail.com>
2026-01-15 17:07:11 +0100

    Fix misspeling

---
+++ WpfApp1/Domain/FileCategoryEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1.Domain
+{
+    enum FileCategoryEnum
+    {
+        Image,
+        Document,
+        Archive,
+        Media
+    }
+}
--- WpfApp1/Domain/FileTypeEnum.cs
+++ WpfApp1/Domain/FileTypeEnum.cs
@@ -2,12 +2,48 @@
 {
     public enum FileTypeEnum
     {
+        // Image
         PNG,
+        JPG,
         JPEG,
-        BMP, // Windows bitmap
+        BMP,
         GIF,
-        XCF, //Gimp
-        PSD, //Adobe photoshop
-        PDF
+        TIFF,
+        TIF,
+        WEBP,
+        PSD,   // Photoshop
+        XCF,   // GIMP
+        AI,    // Adobe Illustrator
+        EPS,   // Vector
+        SVG,    // Vector (XML)
+
+        // DOCS enums
+
+        PDF,
+        DOC,
+        DOCX,
+        XLS,
+        XLSX,
+        PPT,
+        PPTX,
+        TXT,
+
+        // Media enums
+
+        MP3,
+        WAV,
+        FLAC,
+        MP4,
+        AVI,
+        MKV,
+
+        // archives enums
+
+        ZIP,
+        RAR,
+        SevenZ,
+        TAR,
+        GZ
+
     }
 }
